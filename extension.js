644e3ce8d086980c74d8f10f4778affbbf1c595d
anishFE <anish@finnest.io>
2025-03-26 01:42:56 +0530

    initiated

---
+++ extension.js
@@ -0,0 +1,28 @@
+
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	console.log('Congratulations, your extension "inline-console" is now active!');
+
+	const disposable = vscode.commands.registerCommand('inline-console.helloWorld', function () {
+
+		vscode.window.showInformationMessage('Hello World from inline-console!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
